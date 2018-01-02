f7d15a21e07be79bce97292eae0d4f856497e2fd
Rohith Menon <rohithmenon@gmail.com>
2018-01-02 02:25:15 +0000

    Working track1 model

--- behavioral-cloning/behavioral_clone.py
+++ behavioral-cloning/behavioral_clone.py
@@ -1,3 +1,4 @@
+import collections
 import csv
 import cv2
 import numpy as np
@@ -11,31 +12,41 @@ from keras import backend as K
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
+Correction = collections.namedtuple('Correction', 'center left right')
+
 samples = []
+n_augmented_samples = 0
+train_test_split_ratio = 0.1
 # Read driving log given the directory and create samples.
-def add_to_samples(dir_path, correction):
+def add_to_samples(dir_path, center_correction=None, left_correction=None, right_correction=None):
+    global n_augmented_samples
     with open('{}/driving_log.csv'.format(dir_path)) as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
-            samples.append((line, dir_path, correction))
+            samples.append((line, dir_path, Correction(center_correction, left_correction, right_correction)))
+            # Two samples will be added for center, right and left images 
+            n_augmented_samples += 2
+            n_augmented_samples += 2 if left_correction else 0
+            n_augmented_samples += 2 if right_correction else 0
 
 # Add samples from center drive
-add_to_samples('./c_data', None)
+add_to_samples('./track1_center', left_correction=0.1, right_correction=-0.1)
 # Add samples from left side drive
-add_to_samples('./ls_data', 0.5)
+add_to_samples('./track1_left', center_correction=0.2)
 # Add samples from right side drive
-add_to_samples('./rs_data', -0.5)
+add_to_samples('./track1_right', center_correction=-0.2)
 # Add samples from curve track
-#add_to_samples('./curve_c_data', None)
-# Add samples from curve left side drive
-#add_to_samples('./curve_data_ls', 0.5)
-# Add samples from curve right side drive
-#add_to_samples('./curve_data_rs', -0.5)
+#add_to_samples('./track2_center', left_correction=0.2, right_correction=-0.2)
+# Add samples from track2 curves 
+#add_to_samples('./track2_curves')
 
 # Split train/validation samples
-train_samples, validation_samples = train_test_split(samples, test_size=0.1)
+train_samples, validation_samples = train_test_split(samples, test_size=train_test_split_ratio)
 
-def generator(samples, batch_size=32):
+# Expands samples with augmented data.
+# 1. Corrections
+# 2. Flips
+def augment(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         for offset in range(0, num_samples, batch_size):
@@ -45,38 +56,53 @@ def generator(samples, batch_size=32):
             for batch_sample_tuple in batch_samples:
                 batch_sample, dir_path, correction = batch_sample_tuple
                 center_image = cv2.imread('{}/IMG/{}'.format(dir_path, batch_sample[0].split('/')[-1]))
-                center_angle = correction if correction else float(batch_sample[3])
+                center_angle = float(batch_sample[3]) + correction.center if correction.center else 0.0
                 images.append(center_image)
                 angles.append(center_angle)
                 images.append(np.flip(center_image, 1))
                 angles.append(-center_angle)
         
-                if not correction:
+                if correction.left:
                     left_image = cv2.imread('{}/IMG/{}'.format(dir_path, batch_sample[1].split('/')[-1]))
-                    left_angle = center_angle + 0.2
+                    left_angle = center_angle + correction.left
                     images.append(left_image)
                     angles.append(left_angle)
                     images.append(np.flip(left_image, 1))
                     angles.append(-left_angle)
 
+                if correction.right:
                     right_image = cv2.imread('{}/IMG/{}'.format(dir_path, batch_sample[2].split('/')[-1]))
-                    right_angle = center_angle - 0.2
+                    right_angle = center_angle + correction.right
                     images.append(right_image)
                     angles.append(right_angle)
                     images.append(np.flip(right_image, 1))
                     angles.append(-right_angle)
 
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield shuffle(X_train, y_train)
+            yield shuffle(images, angles)
 
+# Generate batches of images with angles from augmented generator
+def generator(samples, batch_size=32):
+    batch_images = []
+    batch_angles = []
+    for images, angles in augment(samples, batch_size):
+        for image, angle in zip(images, angles):
+            batch_images.append(image)
+            batch_angles.append(angle)
+            # If after adding images, we hit the batch_size, return
+            if len(batch_images) == batch_size:
+                X_train = np.array(batch_images)
+                y_train = np.array(batch_angles)
+                batch_images = []
+                batch_angles = []
+                yield (X_train, y_train)
+                 
 # compile and train the model using the generator function
 train_generator = generator(shuffle(train_samples), batch_size=32)
 validation_generator = generator(shuffle(validation_samples), batch_size=32)
 
-#ch, row, col = 3, 80, 320  # Trimmed image format
 ch, row, col = 3, 160, 320 # Trimmed image format
 
+# Nvidia end-to-end self-driving network
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(row, col, ch)))
@@ -96,6 +122,6 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= \
-            len(train_samples) / 32, validation_data=validation_generator, \
-            nb_val_samples=len(validation_samples), nb_epoch=5)
-model.save('model.h5')
+            n_augmented_samples * train_test_split_ratio, validation_data=validation_generator, \
+            nb_val_samples=len(validation_samples), nb_epoch=3)
+model.save('model.t1.h5')
